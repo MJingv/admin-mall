4e63ce39f6d385560db3dc50eddca177c8ea99ec
MJingv <625619954@qq.com>
2018-03-04 18:18:05 +0800

    webpack config

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+import './index.scss'
+import 'font-awesome/css/font-awesome.min.css'
+
+ReactDOM.render(
+    <div>
+        <i className='fa fa-address-book'></i>
+        <div>hello world from react</div>
+    </div>,
+    document.getElementById('app')
+)
