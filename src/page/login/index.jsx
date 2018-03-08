bad0b7aca6a04758909cb7c8d322d50fce7d272f
MJingv <625619954@qq.com>
2018-03-08 21:05:53 +0800

    未完成的样式和ajax

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -1,13 +1,58 @@
+import {Form, Icon, Input, Button} from 'antd';
 import React from 'react'
+import './index.scss'
+
+const {Item} = Form;
 
 export default class  extends React.Component {
-    render() {
-        return (
-            <div>
-                i am login!!!
-            </div>
-        )
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            password: '',
+        };
+    }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(e)
+    }
+
+    render() {
+        const {userName, password} = this.state;
+        return (
+            <div className='background'>
+                <Form className='form-wrap' onSubmit={this.handleSubmit}>
+                    <Item className='form-item'>
+                        <div className='form-logo'>Welcome to ADMIN MALL</div>
+                    </Item>
+                    <Item className='form-item'>
+                        <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                               placeholder="用户名/Username"
+                               onChange={()=>{
+                                   this.setState({
+                                       userName:userName
+                                   })
+                               }}
+                        />
+                    </Item>
+                    <Item className='form-item'>
+                        <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} type="password"
+                               placeholder="密码/Password"
+                               onChange={()=>{
+                                   this.setState({
+                                       password:password
+                                   })
+                               }}/>
+                    </Item>
+                    <Item className='form-item'>
+                        <Button type="primary" htmlType="submit" className="form-button">
+                            登录 Log in
+                        </Button>
+                    </Item>
+
+                </Form>
+            </div>
+        );
     }
 }
-
