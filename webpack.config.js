46d18716b684873c37098c7d2b47964cc75d9d91
MJingv <625619954@qq.com>
2018-03-28 18:16:20 +0800

    增加dist

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/app.js',
-        publicPath: WEBPACK_ENV === 'dev' ? '/dist/' : 'mall.mjehol.cn/dist/',
+        publicPath: WEBPACK_ENV === 'dev' ? '/dist/' : 'http://mall.mjehol.cn/dist/',
     },
     resolve: {
         alias: {
