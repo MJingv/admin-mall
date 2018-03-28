52f32ce5c433ab3f037f8168af5343fa22d21483
MJingv <625619954@qq.com>
2018-03-28 16:56:50 +0800

    修改上线配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,14 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+let WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+
 module.exports = {
     entry: './src/app.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/app.js',
-        publicPath: '/dist/'
+        publicPath: WEBPACK_ENV === 'dev' ? '/dist/' : '//adminmall/mjehol.cn/dist',
     },
     resolve: {
         alias: {
